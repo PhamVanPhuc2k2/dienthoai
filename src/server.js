71e59b8ba3d8207cd56089e3a96607e739461763
PhamVanPhuc2k2 <phamvanphuctnp@gmail.com>
2025-04-09 14:32:01 +0700

    Kết nối tới MySQL

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
+import connectDB from "./config/connectDB";
 
 let app = express();
 
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
