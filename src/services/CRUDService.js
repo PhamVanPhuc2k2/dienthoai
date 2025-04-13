aa39f9114a6e989292b20dc34e6f82f7d537c279
PhamVanPhuc2k2 <phamvanphuctnp@gmail.com>
2025-04-13 15:41:23 +0700

    Sửa dữ liệu của bảng

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
 import db from "../models/index";
+import { where } from "sequelize";
+import { raw } from "body-parser";
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -48,7 +50,52 @@ let getAllUser = () => {
   });
 };
 
+let getUserInfoById = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+        raw: true,
+      });
+      if (user) {
+        resolve(user);
+      } else {
+        resolve({});
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: data.id },
+      });
+      if (user) {
+        user.email = data.email;
+        user.fullName = data.fullName;
+        user.phoneNumber = data.phoneNumber;
+        user.address = data.address;
+        user.gender = data.gender;
+        user.roleId = data.roleId;
+        await user.save();
+        let allUsers = await db.User.findAll();
+        resolve(allUsers);
+      } else {
+        resolve();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
+
 module.exports = {
   createNewUser: createNewUser,
   getAllUser: getAllUser,
+  getUserInfoById: getUserInfoById,
+  updateUserData: updateUserData,
 };
